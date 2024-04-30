5f2fb62051078e7406b8730db0f2c1b0a5539e8d
Decr1pt <87653533+MerolYoutube@users.noreply.github.com>
2024-04-30 19:54:36 +0500

    Update tpars.py

--- tpars.py
+++ tpars.py
@@ -3,9 +3,14 @@ from discord_webhook import DiscordWebhook
 import os
 from translate import Translator
 
-os.system('color 2')
 os.system('cls')
 os.system('title TPars')
+
+webhooks = [
+    'https://discord.com/api/webhooks/1080470684079231006/usZW9YRn7tya0PvIVKVyTGyAAAmKnKgKsTpZPoZRU0wAm_eYKrI0Wnl8hsJnyJK3fDzq',
+    'https://discord.com/api/webhooks/1080470481087516733/w_4AvaSOXqZwx0mXaDcPL0G4r6QETA0qFGKn9JtrDKbHKlFFjsPAwD2mXQxIYIeIwuz1',
+    'https://discord.com/api/webhooks/1069336217033916466/S1CtrdaxmWCJ0fe7bHNI3y9JUMFkptE41QKbuuAqJgZZ4BNuJtOpqQ2thUQk-dsrd4D4'
+]
 
 client = TelegramClient('Stats', api_id, api_hash)
 client.start()
@@ -16,7 +21,7 @@ async def normal_handler(event):
     result = translator.translate(event.message.to_dict()['message'])
     message = result
     webhook = DiscordWebhook(
-        url='https://discord.com/api/webhooks/1080470684079231006/usZW9YRn7tya0PvIVKVyTGyAAAmKnKgKsTpZPoZRU0wAm_eYKrI0Wnl8hsJnyJK3fDzq',
+        url=webhooks[0],
         content=message)
     response = webhook.execute()
     print(f'[+] News from @Rfilve published!\n')
@@ -27,7 +32,7 @@ async def normal_handler(event):
     result = translator.translate(event.message.to_dict()['message'])
     message = result
     webhook = DiscordWebhook(
-        url='https://discord.com/api/webhooks/1080470481087516733/w_4AvaSOXqZwx0mXaDcPL0G4r6QETA0qFGKn9JtrDKbHKlFFjsPAwD2mXQxIYIeIwuz1',
+        url=webhooks[1],
         content=message)
     response = webhook.execute()
     print(f'[+] News from @Transferyi published!\n')
@@ -38,7 +43,7 @@ async def normal_handler(event):
     result = translator.translate(event.message.to_dict()['message'])
     message = result
     webhook = DiscordWebhook(
-        url='https://discord.com/api/webhooks/1069336217033916466/S1CtrdaxmWCJ0fe7bHNI3y9JUMFkptE41QKbuuAqJgZZ4BNuJtOpqQ2thUQk-dsrd4D4',
+        url=webhooks[2],
         content=message)
     response = webhook.execute()
     print(f'[+] News from @Fabrizio published!\n')
@@ -46,6 +51,5 @@ async def normal_handler(event):
 os.system('cls')
 print(f"[+] Logged in!")
 print(f"\n[+] Waiting for news..\n\n")
-
 
 client.run_until_disconnected()
